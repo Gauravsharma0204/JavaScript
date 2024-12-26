6b3e6ac4cba8b156b9c6b2ed890c5cbd2ab22187
Gauravsharma0204 <gauravsh0204@gmail.com>
2024-12-26 17:49:57 +0000

    container config

---
+++ Basic/01_Variable.js
@@ -0,0 +1,18 @@
+const accountId = 144553; // Can't be Change
+let acountEmail = "Gaurav@gmail.com"; //
+
+var accountPassword = "12345"
+// Prefer not to use var
+// Because of issue in block scope and functional scope;
+
+accountCity = "Jaipur";
+
+let accountState;
+
+acountEmail = "gaddghkj@gmail.com";
+accountPassword = "123456789";
+accountCity = "Bangaluru";
+
+console.log(accountId);
+console.table([accountId, accountPassword, acountEmail, accountCity, accountState]);
+
---
+++ Basic/02_DataType.js
@@ -0,0 +1,53 @@
+"use strict" // treat all JS Code as newer version
+
+//alert("Hello"); We Are using Node.js Not Browser.
+
+// Code Readability should be High
+// console.log(3 + 3);
+// console.log("Gaurav");
+
+
+// Data Types
+
+// Number => 2  to power 52
+let num = 12346;
+console.log(num);
+
+// Bigint 
+
+// String => ""
+let str = "Hii Gaurav";
+console.log(str);
+
+// Boolean => true/false
+let bool = true;
+console.log(bool);
+
+// Null => Standalone Value (Type of Null is Object)
+let null_data = null
+console.log(null_data);
+
+// Undefined => 
+let unde_fined;
+    console.log(unde_fined);
+    
+// Symbol => unique
+
+// Object 
+
+console.log(typeof null); // Object
+
+console.log(typeof undefined); // Undefined
+
+console.log(typeof "Gaurav"); // String
+
+console.log(typeof 12345); // Number
+
+console.log(typeof true); // Boolean
+
+
+
+
+
+
+
